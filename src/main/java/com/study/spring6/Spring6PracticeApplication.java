5038c6f1944c655ec4e0101c18fe05b5568d1b55
ghkdusghd <164862216+ghkdusghd@users.noreply.github.com>
2025-02-11 15:50:43 +0900

    first commit

---
+++ src/main/java/com/study/spring6/Spring6PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.study.spring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/spring6/Spring6PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
